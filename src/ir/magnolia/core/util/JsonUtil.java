6a5bf6c5fe6497045aa3710b3702dabfd4a7bc4a
javaee5 <sedagat@123>
2016-10-22 12:54:15 +0330

    first commit

--- src/ir/magnolia/core/util/JsonUtil.java
+++ src/ir/magnolia/core/util/JsonUtil.java
@@ -1,11 +1,12 @@
 package ir.magnolia.core.util;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import ir.magnolia.core.model.entity.*;
+import ir.magnolia.core.model.objectValue.TicketOrder;
 
-import java.util.Arrays;
+import java.util.List;
 
-public class JsonUtil<T> {
+public class JsonUtil {
 
     private static ObjectMapper objectMapper = new ObjectMapper();
 
@@ -13,4 +14,8 @@ public class JsonUtil<T> {
         return objectMapper.writeValueAsString(reserve);
     }
 
+    public static List<TicketOrder> findInJson(String json) throws Exception {
+        return objectMapper.readValue(json, new TypeReference<List<TicketOrder>>() {
+        });
+    }
 }
