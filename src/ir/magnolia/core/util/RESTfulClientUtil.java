fe6f7e38df732f444a7e1cd492fa8317168c625c
javaee5 <sedagat@123>
2016-12-31 00:27:12 +0330

    first commit

--- src/ir/magnolia/core/util/RESTfulClientUtil.java
+++ src/ir/magnolia/core/util/RESTfulClientUtil.java
@@ -38,4 +38,24 @@ public class RESTfulClientUtil {
         }
     }
 
+    public String restFullService(String url, String jsonString) {
+        try {
+            HttpClient client = new DefaultHttpClient();
+            HttpPost postRequest = new HttpPost(url);
+            postRequest.setHeader("Accept", "application/json");
+            postRequest.setHeader("Content-type", "application/json");
+            postRequest.setEntity(new StringEntity(jsonString, "UTF-8"));
+            HttpResponse response = client.execute(postRequest);
+            if (response.getStatusLine().getStatusCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + response.getStatusLine().getStatusCode());
+            }
+            return IOUtils.toString(response.getEntity().getContent(), "UTF-8");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
 }
