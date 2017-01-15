6d1f95d29dee1c6425a81a0f695baa97a2f4a967
javaee5 <sedagat@123>
2017-01-15 18:05:35 +0330

    first commit

--- src/ir/magnolia/core/util/MailUtil.java
+++ src/ir/magnolia/core/util/MailUtil.java
@@ -13,18 +13,14 @@ import java.util.Properties;
 
 public class MailUtil {
 
-    public static void main(String[] args) {
-        MailUtil.sendMail("info@CafeParvaz.ir", "hamedhatami2012@gmail.com", "sample".getBytes(), "subject", "message body");
-    }
-
-    public static boolean sendMail(String from, String to, byte[] file, String subject, String body) {
+    public static boolean sendMail(String to, byte[] file, String subject, String body) {
         try {
             Properties properties = System.getProperties();
-            properties.setProperty("mail.smtp.host", "127.0.0.1");
-            properties.setProperty("mail.smtp.port", "25");
+            properties.setProperty("mail.smtp.host", Configuration.getProperty("mail_host"));
+            properties.setProperty("mail.smtp.port", Configuration.getProperty("mail_port"));
             Session session = Session.getDefaultInstance(properties);
             MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
+            message.setFrom(new InternetAddress(Configuration.getProperty("mail_from")));
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
             message.setSubject(subject);
             BodyPart messageBodyPart = new MimeBodyPart();
@@ -34,12 +30,13 @@ public class MailUtil {
             messageBodyPart = new MimeBodyPart();
             DataSource source = new ByteArrayDataSource(file, "application/pdf");
             messageBodyPart.setDataHandler(new DataHandler(source));
-            messageBodyPart.setFileName("CafeParvaz.pdf");
+            messageBodyPart.setFileName(Configuration.getProperty("mail_filename"));
             multipart.addBodyPart(messageBodyPart);
             message.setContent(multipart);
             Transport.send(message);
             return true;
         } catch (Exception mex) {
+            mex.printStackTrace();
             return false;
         }
     }
